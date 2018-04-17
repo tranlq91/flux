cfdb95b7111f28b652ddff9ad6e3ed34c798866a
tranlq91 <tranlq91@gmail.com>
2018-04-17 18:59:07 +0700

    build structure demo

---
+++ examples/my-todomvc/src/data/TodoStore.js
@@ -0,0 +1,27 @@
+import Immutable from 'immutable';
+import {ReduceStore} from 'flux/utils';
+import TodoActionTypes from './TodoActionTypes';
+import TodoDispatcher from './TodoDispatcher';
+
+class TodoStore extends ReduceStore {
+  constructor() {
+    super(TodoDispatcher);
+  }
+
+  getInitialState() {
+    return Immutable.OrderedMap();
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case TodoActionTypes.ADD_TODO:
+        // Do nothing for now, we will add logic here soon!
+        return state;
+
+      default:
+        return state;
+    }
+  }
+}
+
+export default new TodoStore();
